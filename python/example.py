bb5e954b8275327be7fc75f91d95fd6a3380c2c5
Ignacio Tartavull <tartavull@gmail.com>
2017-01-13 14:37:16 -0500

    Modify example to display snemi3d EM image and segmentation.

--- python/example.py
+++ python/example.py
@@ -2,15 +2,7 @@ from __future__ import print_function
 
 import neuroglancer
 import numpy as np
-
-a = np.zeros((3, 100, 100, 100), dtype=np.uint8)
-ix, iy, iz = np.meshgrid(*[np.linspace(0, 1, n) for n in a.shape[1:]], indexing='ij')
-a[0, :, :, :] = np.abs(np.sin(4 * (ix + iy))) * 255
-a[1, :, :, :] = np.abs(np.sin(4 * (iy + iz))) * 255
-a[2, :, :, :] = np.abs(np.sin(4 * (ix + iz))) * 255
-
-b = np.cast[np.uint32](np.floor(np.sqrt((ix - 0.5)**2 + (iy - 0.5)**2 + (iz - 0.5)**2) * 10))
-b = np.pad(b, 1, 'constant')
+import h5py
 
 # Obtain the bundled Neuroglancer client code (HTML, CSS, and JavaScript) from
 # the demo server, so that this example works even if
@@ -20,18 +12,11 @@ b = np.pad(b, 1, 'constant')
 # has not been run.
 neuroglancer.set_static_content_source(url='https://neuroglancer-demo.appspot.com')
 
-viewer = neuroglancer.Viewer(voxel_size=[10, 10, 10])
-viewer.add(a,
-           name='a',
-           # offset is in nm, not voxels
-           offset=(200, 300, 150),
-           shader="""
-void main() {
-  emitRGB(vec3(toNormalized(getDataValue(0)),
-               toNormalized(getDataValue(1)),
-               toNormalized(getDataValue(2))));
-}
-""")
-viewer.add(b, name='b')
+viewer = neuroglancer.Viewer(voxel_size=[6, 6, 40])
+with h5py.File('./snemi3d/image.h5') as f:
+  viewer.add(f['main'][:], name='image')
+
+with h5py.File('./snemi3d/machine_labels.h5') as f:
+  viewer.add(f['main'][:], name='segmentation')
 print(viewer)
 input()
