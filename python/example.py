0176def74c2dbaadcf522b26f177b9ddf2857dc3
Ignacio Tartavull <tartavull@gmail.com>
2017-01-24 16:45:31 -0500

    The annotation user layer was clone and slightly adapted for synapse annotation. The point list was modified to remove two elements simultaneusly instead of one. The point color depends on it being even or odd. A new shader draws lines between pre and post points. There is probably oportunities to remove duplicated code.

--- python/example.py
+++ python/example.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 
-import neuroglancer
 import numpy as np
 import h5py
+import webbrowser
+
+import neuroglancer
 
 # Obtain the bundled Neuroglancer client code (HTML, CSS, and JavaScript) from
 # the demo server, so that this example works even if
@@ -12,11 +14,23 @@ import h5py
 # has not been run.
 neuroglancer.set_static_content_source(url='http://localhost:8080')
 
+def add_point_annotation(viewer, name='annotation'):
+  empty_data = np.zeros(shape=[1,1,1],dtype=np.float32)
+  viewer.add(empty_data, name=name, volume_type='pointAnnotation')
+
+def add_synapse_annotation(viewer, name='annotation'):
+  empty_data = np.zeros(shape=[1,1,1],dtype=np.float32)
+  viewer.add(empty_data, name=name, volume_type='synapseAnnotation')
+
 viewer = neuroglancer.Viewer(voxel_size=[6, 6, 40])
 with h5py.File('./snemi3d/image.h5') as f:
   viewer.add(f['main'][:], name='image')
 
-with h5py.File('./snemi3d/machine_labels.h5') as f:
-  viewer.add(f['main'][:], name='segmentation')
-print(viewer)
-input()
+# add_point_annotation(viewer)
+add_synapse_annotation(viewer)
+
+# with h5py.File('./snemi3d/machine_labels.h5') as f:
+#   viewer.add(f['main'][:], name='segmentation')
+
+webbrowser.open(viewer.get_viewer_url())
+print(viewer.get_viewer_url())
