84a074205cef5b9c9e6323d953f54bcfe4ca8ba9
William Silversmith <wms@mit.edu>
2017-02-08 10:47:40 -0500

    Updated upload.py to use gsutil for fast upload

--- python/upload.py
+++ python/upload.py
@@ -7,14 +7,21 @@ from neuroglancer import chunks
 from google.cloud import storage
 import zlib
 from tqdm import tqdm
+import subprocess
+import shutil
+
 
 client = storage.Client(project='neuromancer-seung-import')
 bucket = client.get_bucket('neuroglancer-dev')
 
 DATASET_NAME = 'snemi3d'
 
-def eachBlob(img, info, layer_name):
+def generateStagingMaterials(img, info, layer_name):
   for scale in info["scales"]:
+
+    staging_dir = './staging/' + layer_name + '/' + scale['key']
+    os.makedirs(staging_dir)
+
     for chunk_size in scale["chunk_sizes"]:
       n_chunks = np.array(scale["size"]) / np.array(chunk_size)
       
@@ -42,40 +49,45 @@ def eachBlob(img, info, layer_name):
         elif scale["encoding"] == "raw":
           content_encoding = 'gzip'
           encoded = chunks.encode_raw(img_chunk)
-          encoded = zlib.compress(encoded, 8)
+          # encoded = zlib.compress(encoded, 8)
         else:
           raise NotImplemented
 
-        filename = dirname + '{}-{}_{}-{}_{}-{}'.format(
+        filename = '{}-{}_{}-{}_{}-{}'.format(
           x * x_chunk_size, (x + 1) * x_chunk_size,
           y * y_chunk_size, (y + 1) * y_chunk_size,
           z * z_chunk_size, (z + 1) * z_chunk_size
         ) 
 
-        blob = storage.blob.Blob(filename, bucket)
-        blob.content_encoding = content_encoding
-        blob.content_type = content_type
-        blob.cache_control = 'no-cache'
+        # blob = storage.blob.Blob(dirname + filename, bucket)
+        # blob.content_encoding = content_encoding
+        # blob.content_type = content_type
+        # blob.cache_control = 'no-cache'
 
-        yield (blob, encoded)
+        with open(staging_dir + '/' + filename, 'w+') as f:
+          f.write(encoded)
+
+    yield staging_dir, scale['key']
 
 
 def upload_hdf5(hdf5_filename, info, layer):
-  print("Uploading " + layer)
-
+  print("Processing " + layer)
   with h5py.File(hdf5_filename) as f:
     img = f['main']
 
+    print("Uploading Info")
     blob = storage.blob.Blob('{}/{}/info'.format(DATASET_NAME, layer), bucket)
     blob.upload_from_string(json.dumps(info))
     blob.make_public()
 
-    for blob, encoded in eachBlob(img, info, layer):
-        blob.upload_from_string(encoded, blob.content_type)
-        blob.make_public()
-          
-    for blob, encoded in eachBlob(img, info, layer):
-        blob.patch()
+    print("Generating staging files...")
+    for staging_dir, key in generateStagingMaterials(img, info, layer):
+      print("Uploading " + key)
+      gsutil_upload_command = "gsutil -m cp -Z -a public-read {}/* gs://neuroglancer-dev/snemi3d/{}/{}/".format(
+        staging_dir, layer, key
+      )
+      print(gsutil_upload_command)
+      subprocess.call(gsutil_upload_command, shell=True)  
 
 chunk_size = 64
 isotropic_chunk_size = [ chunk_size, chunk_size, chunk_size ]
@@ -109,6 +121,8 @@ segmentation_info = {
   }], 
   "type": "segmentation",
 }
+
+# shutil.rmtree('./staging')
 
 # upload_hdf5('./snemi3d/image.h5', image_info, 'image')
 upload_hdf5('./snemi3d/machine_labels.h5', segmentation_info, 'segmentation')
