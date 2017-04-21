27064a9001ec239046230c13f8db3f2f6dc31e9d
jingpengwu <jingpeng.wu@gmail.com>
2017-04-21 15:47:52 -0400

    create compute environment and job-queue

--- python/ext/third_party/awsbatch/tasks/inference.py
+++ python/ext/third_party/awsbatch/tasks/inference.py
@@ -1,22 +1,68 @@
+#!/usr/bin/env python
+
 import boto3
 
 client = boto3.client('batch')
 
 resp = client.submit_job(
-    jobName         = 'test',
-    jobQueue        = 'inference',
-    jobDefinition   = 'inference:2',
-    containerProperties = {
-        'image'     : '098703261575.dkr.ecr.us-east-1.amazonaws.com/chunkflow:v1.5.4',
+    jobName         = 'pinky40-inference',
+    jobQueue        = 'convnet-inference-4',
+    jobDefinition   = 'inference:3',
+    parameters      = {
+        'queuename'     : 'pinky-inference',
+        'processnumber' : '4',
+        'workernumber'  : '1',
+        'waittime'      : '5'
+    },
+    containerOverrides = {
         'command'   : [
             "julia",
-            "/root/.julia/v0.5/ChunkFlow/scripts/main.jl"
+            "-p", "Ref::processnumber",
+            "/root/.julia/v0.5/ChunkFlow/scripts/main.jl",
+            "-q", "Ref::queuename",
+            "-n", "Ref::workernumber",
+            "-w", "Ref::waittime"
         ],
-        'vcpus'     : 4,
+        'vcpus'     : 2,
         'memory'    : 30500
     },
     retryStrategy   = {'attempts' : 3})
-resp
 
-resp = client.describe_compute_environments()
+
+resp = client.create_compute_environment(
+    computeEnvironmentName  = 'convnet-inference-4',
+    type                    = 'MANAGED',
+    state                   = 'ENABLED',
+    computeResources        = {
+        'type'              : 'SPOT',
+        'minvCpus'          : 0,
+        'maxvCpus'          : 4000,
+        'desiredvCpus'      : 2,
+        'instanceTypes'     : [ 'p2.xlarge', ],
+        'imageId'           : 'ami-06ae2e10',
+        'subnets'           : ['subnet-fb3626a0',],
+        'securityGroupIds'  : ['sg-35753f4a',],
+        'ec2KeyPair'        : 'jpwu_workstation',
+        'instanceRole'      : 'arn:aws:iam::098703261575:role/chunkflow-worker',
+        # spot compute environment do not support tagging!
+        # 'tags'              : {
+        #     'User'          : 'jingpeng',
+        #     'Project'       : 'pinky40'
+        # },
+        'bidPercentage'     : 91,
+        'spotIamFleetRole'  : 'arn:aws:iam::098703261575:role/aws-ec2-spot-fleet-role'
+    },
+    serviceRole             = 'arn:aws:iam::098703261575:role/service-role/AWSBatchServiceRole')
 resp
+resp = client.describe_compute_environments()
+
+resp = client.create_job_queue(
+    jobQueueName                    = 'convnet-inference-4',
+    state                           = 'ENABLED',
+    priority                        = 50,
+    computeEnvironmentOrder = [
+        {
+            'order'                 : 1,
+            'computeEnvironment'    : 'convnet-inference-4'
+        }
+    ])
