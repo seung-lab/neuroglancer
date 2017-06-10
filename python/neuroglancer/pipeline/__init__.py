0911f838e54c5453d5593f2165909dc3729f3845
Jonathan Zung <jonathanzung@gmail.com>
2017-06-10 17:42:52 -0500

    -

--- python/neuroglancer/pipeline/__init__.py
+++ python/neuroglancer/pipeline/__init__.py
@@ -1,7 +1,7 @@
 from neuroglancer._mesher import Mesher
 from storage import Storage
 from precomputed import Precomputed, EmptyVolumeException
-from task_queue import TaskQueue, RegisteredTask
+from task_queue import *
 from tasks import *
 from tasks_watershed import WatershedTask
 from task_yacn import *
