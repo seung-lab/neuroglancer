73e67dd7419ab1013e4cb13e8ea4ac783a47125c
Jeremy Maitin-Shepard <jbms@google.com>
2017-07-06 14:36:08 -0700

    fix: make SliceViewPanel and PerspectivePanel resize handling more robust.
    For various reasons the client area of a panel may change without a resize event.  This resulted in
    drawing sometimes being done with the wrong dimensions, resulting in certain artifacts.  This is
    avoided by always checking for a possible resize prior to drawing.

--- src/neuroglancer/perspective_view/panel.ts
+++ src/neuroglancer/perspective_view/panel.ts
@@ -179,9 +179,12 @@ export class PerspectivePanel extends RenderedDataPanel {
   }
 
   onResize() {
-    this.width = this.element.clientWidth;
-    this.height = this.element.clientHeight;
-    this.viewportChanged();
+    const {clientWidth, clientHeight} = this.element;
+    if (clientWidth !== this.width || clientHeight !== this.height) {
+      this.width = this.element.clientWidth;
+      this.height = this.element.clientHeight;
+      this.viewportChanged();
+    }
   }
 
   disposed() {
@@ -256,6 +259,7 @@ export class PerspectivePanel extends RenderedDataPanel {
     if (!this.navigationState.valid || width === 0 || height === 0) {
       return;
     }
+    this.onResize();
 
     if (this.viewer.showSliceViews.value) {
       for (let sliceView of this.sliceViews) {
