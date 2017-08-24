48a9fa831e4841f2cc0e641716e8a1a697cc63ae
William Silversmith <william.silversmith@gmail.com>
2017-08-24 14:45:11 -0400

    fix: python example server back online (#170)
    * fix: python example server back online
    
    Several issues fixed:
    - missing invocation of ./node_modules/webpack/bin/webpack.js in package.json scripts
    - incorrect invocation of URL rather than file path in example.py
    - passing None to file_open, resulting in no file handle being available (unable to call None)
    - many appearently missing modules from package.json
    
    Added:
    - block to server.py that allows for invocation of example.py as `python example.py` rather than `python -i example.py`
    
    * chore: added npm shrinkwrap file
    
    * fix(example.py): script works with both -i and not
    
    Also respects ctrl-C in all cases.
    
    * fix: moving requirements around to satisfy travis
    
    * fix: npm run build-min requires Java 7+
    
    google-closure-compiler was dying if Java 1.6 was installed with error
    EPIPE.
    
    * fix: added missing type coersion to chunk_manager/backend.ts

--- python/example.py
+++ python/example.py
@@ -1,16 +1,19 @@
-from __future__ import print_function
 
 import numpy as np
 import h5py
 import webbrowser
 import neuroglancer
+from neuroglancer import static
+import sys
 
+path = './neuroglancer/static/'
+
+neuroglancer.set_static_content_source(path=path)
 viewer = neuroglancer.Viewer()
-neuroglancer.set_static_content_source(url='http://localhost:8080')
 
-viewer.add(volume_type='image', data=np.zeros(shape=(100,100,100), dtype=np.uint8), name='image', voxel_size=[6, 6, 40])
-viewer.add(volume_type='point', name='point')
-viewer.add(volume_type='synapse', name='synapse')
+# viewer.add(volume_type='image', data=np.zeros(shape=(100,100,100), dtype=np.uint8), name='image', voxel_size=[6, 6, 40])
+# viewer.add(volume_type='point', name='point')
+# viewer.add(volume_type='synapse', name='synapse')
 
 # f = h5py.File('./snemi3d/machine_labels.h5')
 # # 0 pad is useful to make the meshes that are in contact with the borders
@@ -24,4 +27,11 @@ viewer.add(volume_type='synapse', name='synapse')
 #   graph='./snemi3d/snemi3d_graph.pickle'
 # )
 
-webbrowser.open(viewer.get_viewer_url())
+url = viewer.get_viewer_url()
+webbrowser.open(url)
+print(url)
+
+if not sys.flags.interactive:
+  neuroglancer.block()
+
+
