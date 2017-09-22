024ef69d9bf08d153ac1e5ffecb9025820bc74ac
jingpengwu <jingpeng.wu@gmail.com>
2017-09-22 15:40:04 -0400

    add some example data for layers

--- python/example.py
+++ python/example.py
@@ -22,7 +22,7 @@ neuroglancer.set_static_content_source(path=os.path.join(os.path.dirname(__file_
 viewer = neuroglancer.Viewer(voxel_size=[10, 10, 10])
 
 
-#Fist layer (image)
+#First layer (image)
 a = np.zeros((3, 100, 100, 100), dtype=np.uint8)
 ix, iy, iz = np.meshgrid(*[np.linspace(0, 1, n) for n in a.shape[1:]], indexing='ij')
 a[0, :, :, :] = np.abs(np.sin(4 * (ix + iy))) * 255
@@ -47,15 +47,24 @@ b = np.pad(b, 1, 'constant')
 viewer.add(b, name='b')
 
 #Third layer (points)
-viewer.add(data=[], name='layer_point', layer_type='point')
+viewer.add(data=[[5,8,10],[30,23,21]], name='layer_point', layer_type='point')
 
 #Fourth layer (synapses)
-viewer.add(data=[], name='layer_synapse', layer_type='synapse')
+# the 1D vector of coordinates will be decomposed to coordinate pairs, so you
+# need some duplicated nodes to define a tree
+viewer.add(data=[[0,0,0],[1,3,5], [3,6,9],[5,34,24]],
+                               name='layer_synapse', layer_type='synapse')
 
 #Fith layer (vector graphics)
 viewer.add(data=[[10, 20 ,30],
                  [15, 20 ,30],
                  [15, 25 ,30]], name='layer_vector_graphics', layer_type='line')
+
+# Sixth layer (skeleton)
+skeleton = Skeleton([[10, 20, 30],
+                     [15, 20, 30],
+                     [15, 25, 30]], [[1,2],[2,3]])
+#viewer.add(name='skeleton', layer_type='single_mesh', data = skeleton)
 
 # Open browser
 webbrowser.open(str(viewer))
