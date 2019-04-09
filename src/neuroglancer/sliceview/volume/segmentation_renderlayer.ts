StructuralNeurobiologyLab/SyConn-Neuroglancer

refactor: use WebGL 2 integer operations and 3-d textures
This avoids the need for cumbersome and inefficient emulation of integer operations using floating
point.

This also avoids the need to emulate 3-d textures using 2-d textures.

The hash function family used for segmentation coloring and for the segment hash tables is now based
on Murmur, rather than the very low quality hash function implemented using floating point
operations that was used previously.

src/neuroglancer/sliceview/volume/segmentation_renderlayer.ts
@@ -27,9 +27,6 @@ import {TrackableAlphaValue} from 'neuroglancer/trackable_alpha';
 import {TrackableBoolean} from 'neuroglancer/trackable_boolean';
 import {DisjointUint64Sets} from 'neuroglancer/util/disjoint_sets';
 import {ShaderBuilder, ShaderProgram} from 'neuroglancer/webgl/shader';
-import {glsl_unnormalizeUint8} from 'neuroglancer/webgl/shader_lib';
-
-const selectedSegmentForShader = new Float32Array(8);
 
 export class EquivalencesHashMap {
   generation = Number.NaN;
@@ -136,12 +133,11 @@ uint64_t getMappedObjectId() {
 `);
     }
     this.segmentColorShaderManager.defineShader(builder);
-    builder.addUniform('highp vec4', 'uSelectedSegment', 2);
-    builder.addUniform('highp float', 'uShowAllSegments');
+    builder.addUniform('highp uvec2', 'uSelectedSegment');
+    builder.addUniform('highp uint', 'uShowAllSegments');
     builder.addUniform('highp float', 'uSelectedAlpha');
     builder.addUniform('highp float', 'uNotSelectedAlpha');
     builder.addUniform('highp float', 'uSaturation');
-    builder.addFragmentCode(glsl_unnormalizeUint8);
     let fragmentMain = `
   uint64_t value = getMappedObjectId();
 
@@ -150,16 +146,15 @@ uint64_t getMappedObjectId() {
 `;
     if (this.displayState.hideSegmentZero.value) {
       fragmentMain += `
-  if (value.low == vec4(0,0,0,0) && value.high == vec4(0,0,0,0)) {
+  if (value.value[0] == 0u && value.value[1] == 0u) {
     emit(vec4(vec4(0, 0, 0, 0)));
     return;
   }
 `;
     }
     fragmentMain += `
-  bool has = uShowAllSegments > 0.0 ? true : ${this.hashTableManager.hasFunctionName}(value);
-  if (uSelectedSegment[0] == unnormalizeUint8(value.low) &&
-      uSelectedSegment[1] == unnormalizeUint8(value.high)) {
+  bool has = uShowAllSegments != 0u ? true : ${this.hashTableManager.hasFunctionName}(value);
+  if (uSelectedSegment == value.value) {
     saturation = has ? 0.5 : 0.75;
   } else if (!has) {
     alpha = uNotSelectedAlpha;
@@ -167,15 +162,15 @@ uint64_t getMappedObjectId() {
   vec3 rgb = segmentColorHash(value);
   `;
 
-  // Override color for all highlighted segments.
-  fragmentMain += `
+    // Override color for all highlighted segments.
+    fragmentMain += `
   if(${this.hashTableManagerHighlighted.hasFunctionName}(value)) {
     rgb = vec3(0.2,0.2,2.0);
     saturation = 1.0;
   };
 `;
 
-  fragmentMain += `
+    fragmentMain += `
   emit(vec4(mix(vec3(1.0,1.0,1.0), rgb, saturation), alpha));
 `;
     builder.setFragmentMain(fragmentMain);
@@ -190,21 +185,17 @@ uint64_t getMappedObjectId() {
 
     let {displayState} = this;
     let {segmentSelectionState, visibleSegments} = this.displayState;
-    if (!segmentSelectionState.hasSelectedSegment) {
-      selectedSegmentForShader.fill(0);
-    } else {
+    let selectedSegmentLow = 0, selectedSegmentHigh = 0;
+    if (segmentSelectionState.hasSelectedSegment) {
       let seg = segmentSelectionState.selectedSegment;
-      let low = seg.low, high = seg.high;
-      for (let i = 0; i < 4; ++i) {
-        selectedSegmentForShader[i] = ((low >> (8 * i)) & 0xFF);
-        selectedSegmentForShader[4 + i] = ((high >> (8 * i)) & 0xFF);
-      }
+      selectedSegmentLow = seg.low;
+      selectedSegmentHigh = seg.high;
     }
     gl.uniform1f(shader.uniform('uSelectedAlpha'), this.displayState.selectedAlpha.value);
     gl.uniform1f(shader.uniform('uSaturation'), this.displayState.saturation.value);
     gl.uniform1f(shader.uniform('uNotSelectedAlpha'), this.displayState.notSelectedAlpha.value);
-    gl.uniform4fv(shader.uniform('uSelectedSegment'), selectedSegmentForShader);
-    gl.uniform1f(shader.uniform('uShowAllSegments'), visibleSegments.hashTable.size ? 0.0 : 1.0);
+    gl.uniform2ui(shader.uniform('uSelectedSegment'), selectedSegmentLow, selectedSegmentHigh);
+    gl.uniform1ui(shader.uniform('uShowAllSegments'), visibleSegments.hashTable.size ? 0 : 1);
     this.hashTableManager.enable(gl, shader, this.gpuHashTable);
     this.hashTableManagerHighlighted.enable(gl, shader, this.gpuHashTableHighlighted);
     if (this.hasEquivalences) {

seung-lab/neuroglancer

src/neuroglancer/sliceview/volume/segmentation_renderlayer.ts
@@ -28,10 +28,6 @@ import {TrackableMIPLevelConstraints} from 'neuroglancer/trackable_mip_level_con
 import {TrackableBoolean} from 'neuroglancer/trackable_boolean';
 import {DisjointUint64Sets} from 'neuroglancer/util/disjoint_sets';
 import {ShaderBuilder, ShaderProgram} from 'neuroglancer/webgl/shader';
-import {glsl_unnormalizeUint8} from 'neuroglancer/webgl/shader_lib';
-
-const selectedSegmentForShader = new Float32Array(8);
-const rawSelectedSegmentForShader = new Float32Array(8);
 
 export class EquivalencesHashMap {
   generation = Number.NaN;
@@ -140,13 +136,12 @@ uint64_t getMappedObjectId() {
 `);
     }
     this.segmentColorShaderManager.defineShader(builder);
-    builder.addUniform('highp vec4', 'uSelectedSegment', 2);
-    builder.addUniform('highp vec4', 'uRawSelectedSegment', 2);
-    builder.addUniform('highp float', 'uShowAllSegments');
+    builder.addUniform('highp uvec2', 'uSelectedSegment');
+    builder.addUniform('highp uvec2', 'uRawSelectedSegment');
+    builder.addUniform('highp uint', 'uShowAllSegments');
     builder.addUniform('highp float', 'uSelectedAlpha');
     builder.addUniform('highp float', 'uNotSelectedAlpha');
     builder.addUniform('highp float', 'uSaturation');
-    builder.addFragmentCode(glsl_unnormalizeUint8);
     let fragmentMain = `
   uint64_t value = getMappedObjectId();
   uint64_t rawValue = getUint64DataValue();
@@ -156,19 +151,17 @@ uint64_t getMappedObjectId() {
 `;
     if (this.displayState.hideSegmentZero.value) {
       fragmentMain += `
-  if (value.low == vec4(0,0,0,0) && value.high == vec4(0,0,0,0)) {
+  if (value.value[0] == 0u && value.value[1] == 0u) {
     emit(vec4(vec4(0, 0, 0, 0)));
     return;
   }
 `;
     }
     fragmentMain += `
-  bool has = uShowAllSegments > 0.0 ? true : ${this.hashTableManager.hasFunctionName}(value);
-  if (uSelectedSegment[0] == unnormalizeUint8(value.low) &&
-      uSelectedSegment[1] == unnormalizeUint8(value.high)) {
+  bool has = uShowAllSegments != 0u ? true : ${this.hashTableManager.hasFunctionName}(value);
+  if (uSelectedSegment == value.value) {
     saturation = has ? 0.5 : 0.75;
-    if (uRawSelectedSegment[0] == unnormalizeUint8(rawValue.low) &&
-        uRawSelectedSegment[1] == unnormalizeUint8(rawValue.high)) {
+    if (uRawSelectedSegment == rawValue.value) {
       saturation *= 1.0/4.0;
     }
   } else if (!has) {
@@ -177,7 +170,7 @@ uint64_t getMappedObjectId() {
   vec3 rgb = segmentColorHash(value);
 `;
 
-  // Override color for all highlighted segments.
+    // Override color for all highlighted segments.
     fragmentMain += `
   if(${this.hashTableManagerHighlighted.hasFunctionName}(value)) {
     rgb = vec3(0.2,0.2,2.0);
@@ -200,29 +193,22 @@ uint64_t getMappedObjectId() {
 
     let {displayState} = this;
     let {segmentSelectionState, rootSegments} = this.displayState;
-    if (!segmentSelectionState.hasSelectedSegment) {
-      selectedSegmentForShader.fill(0);
-      rawSelectedSegmentForShader.fill(0);
-    } else {
+    let selectedSegmentLow = 0, selectedSegmentHigh = 0;
+    let rawSelectedSegmentLow = 0, rawSelectedSegmentHigh = 0;
+    if (segmentSelectionState.hasSelectedSegment) {
       let seg = segmentSelectionState.selectedSegment;
-      let low = seg.low, high = seg.high;
-      for (let i = 0; i < 4; ++i) {
-        selectedSegmentForShader[i] = ((low >> (8 * i)) & 0xFF);
-        selectedSegmentForShader[4 + i] = ((high >> (8 * i)) & 0xFF);
-      }
-      seg = segmentSelectionState.rawSelectedSegment;
-      low = seg.low, high = seg.high;
-      for (let i = 0; i < 4; ++i) {
-        rawSelectedSegmentForShader[i] = ((low >> (8 * i)) & 0xFF);
-        rawSelectedSegmentForShader[4 + i] = ((high >> (8 * i)) & 0xFF);
-      }
+      selectedSegmentLow = seg.low;
+      selectedSegmentHigh = seg.high;
+      let rawSeg = segmentSelectionState.rawSelectedSegment;
+      rawSelectedSegmentLow = rawSeg.low;
+      rawSelectedSegmentHigh = rawSeg.high;
     }
     gl.uniform1f(shader.uniform('uSelectedAlpha'), this.displayState.selectedAlpha.value);
     gl.uniform1f(shader.uniform('uSaturation'), this.displayState.saturation.value);
     gl.uniform1f(shader.uniform('uNotSelectedAlpha'), this.displayState.notSelectedAlpha.value);
-    gl.uniform4fv(shader.uniform('uSelectedSegment'), selectedSegmentForShader);
-    gl.uniform4fv(shader.uniform('uRawSelectedSegment'), rawSelectedSegmentForShader);
-    gl.uniform1f(shader.uniform('uShowAllSegments'), rootSegments.hashTable.size ? 0.0 : 1.0);
+    gl.uniform2ui(shader.uniform('uSelectedSegment'), selectedSegmentLow, selectedSegmentHigh);
+    gl.uniform2ui(shader.uniform('uRawSelectedSegment'), rawSelectedSegmentLow, rawSelectedSegmentHigh);
+    gl.uniform1ui(shader.uniform('uShowAllSegments'), rootSegments.hashTable.size ? 0 : 1);
     this.hashTableManager.enable(gl, shader, this.gpuHashTable);
     this.hashTableManagerHighlighted.enable(gl, shader, this.gpuHashTableHighlighted);
     if (this.hasEquivalences) {