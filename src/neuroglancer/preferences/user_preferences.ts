7f3ab3b3041785c9a90783690bd260a217bcfe5e
Akhilesh Halageri <akhileshhalageri@gmail.com>
2019-04-10 16:24:53 -0400

    cosmetics

--- src/neuroglancer/preferences/user_preferences.ts
+++ src/neuroglancer/preferences/user_preferences.ts
@@ -1,7 +1,8 @@
 import {Overlay} from 'neuroglancer/overlay';
 import {Viewer} from 'neuroglancer/viewer';
 import {TrackableBoolean, TrackableBooleanCheckbox} from 'neuroglancer/trackable_boolean';
-
+import {TrackableValue} from 'neuroglancer/trackable_value';
+import {NumberInputWidget} from 'neuroglancer/widget/number_input_widget';
 
 require('./user_preferences.css');
 
@@ -16,6 +17,17 @@ export class UserPreferencesDialog extends Overlay {
     scroll.classList.add('user-preferences-container');
 
     content.appendChild(scroll);
+
+    const addLimitWidget = (label: string, limit: TrackableValue<number>) => {
+      const widget = this.registerDisposer(new NumberInputWidget(limit, {label}));
+      widget.element.classList.add('user-preferences-limit-widget');
+      scroll.appendChild(widget.element);
+      scroll.appendChild(document.createElement('br'));
+    };
+    addLimitWidget('GPU memory limit', viewer.chunkQueueManager.capacities.gpuMemory.sizeLimit);
+    addLimitWidget('System memory limit', viewer.chunkQueueManager.capacities.systemMemory.sizeLimit);
+    addLimitWidget(
+        'Concurrent chunk requests', viewer.chunkQueueManager.capacities.download.itemLimit);
 
     const addCheckbox = (label: string, value: TrackableBoolean) => {
       const labelElement = document.createElement('label');
