527d542379334ff739779f08ac2db1ec0e6cc4d7
Seun Ogedengbe <ooo2@seungworkstation35.princeton.edu>
2019-07-18 09:48:10 -0400

    reverted context.js, removed axios and validator

--- src/neuroglancer/webgl/context.ts
+++ src/neuroglancer/webgl/context.ts
@@ -14,57 +14,57 @@
  * limitations under the License.
  */
 
- import {RefCounted} from 'neuroglancer/util/disposable';
- import {Memoize} from 'neuroglancer/util/memoize';
- 
- export interface GL extends WebGL2RenderingContext {
-   memoize: Memoize<any, RefCounted>;
-   maxTextureSize: number;
-   maxTextureImageUnits: number;
-   tempTextureUnit: number;
- }
- 
- export const DEBUG_SHADERS = false;
- 
- export function initializeWebGL(canvas: HTMLCanvasElement) {
-   let options: any = {
-     'antialias': false,
-     'stencil': true,
-   };
-   if (DEBUG_SHADERS) {
-     console.log('DEBUGGING via preserveDrawingBuffer');
-     options['preserveDrawingBuffer'] = true;
-   }
-   let gl =
-       <GL>canvas.getContext('webgl2', options);
-   if (gl == null) {
-     throw new Error('WebGL not supported.');
-   }
-   gl.memoize = new Memoize<any, RefCounted>();
-   gl.maxTextureSize = gl.getParameter(gl.MAX_TEXTURE_SIZE);
-   gl.maxTextureImageUnits = gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS);
-   gl.tempTextureUnit = gl.maxTextureImageUnits - 1;
- 
-   // FIXME: verify that we received a stencil buffer
-   // var contextAttributes = gl.getContextAttributes();
-   // var haveStencilBuffer = contextAttributes.stencil;
- 
-   for (const extension of ['EXT_color_buffer_float']) {
-     if (!gl.getExtension(extension)) {
-       throw new Error(`${extension} extension not available`);
-     }
-   }
- 
-   // Extensions to attempt to add but not fail if they are not available.
-   for (const extension of [
-            // Some versions of Firefox 67.0 seem to require this extension being added in addition
-            // to EXT_color_buffer_float, despite the note here indicating it is unnecessary:
-            // https://developer.mozilla.org/en-US/docs/Web/API/EXT_float_blend
-            //
-            // See https://github.com/google/neuroglancer/issues/140
-            'EXT_float_blend',
-   ]) {
-     gl.getExtension(extension);
-   }
-   return gl;
- }
+import {RefCounted} from 'neuroglancer/util/disposable';
+import {Memoize} from 'neuroglancer/util/memoize';
+
+export interface GL extends WebGL2RenderingContext {
+  memoize: Memoize<any, RefCounted>;
+  maxTextureSize: number;
+  maxTextureImageUnits: number;
+  tempTextureUnit: number;
+}
+
+export const DEBUG_SHADERS = false;
+
+export function initializeWebGL(canvas: HTMLCanvasElement) {
+  let options: any = {
+    'antialias': false,
+    'stencil': true,
+  };
+  if (DEBUG_SHADERS) {
+    console.log('DEBUGGING via preserveDrawingBuffer');
+    options['preserveDrawingBuffer'] = true;
+  }
+  let gl =
+      <GL>canvas.getContext('webgl2', options);
+  if (gl == null) {
+    throw new Error('WebGL not supported.');
+  }
+  gl.memoize = new Memoize<any, RefCounted>();
+  gl.maxTextureSize = gl.getParameter(gl.MAX_TEXTURE_SIZE);
+  gl.maxTextureImageUnits = gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS);
+  gl.tempTextureUnit = gl.maxTextureImageUnits - 1;
+
+  // FIXME: verify that we received a stencil buffer
+  // var contextAttributes = gl.getContextAttributes();
+  // var haveStencilBuffer = contextAttributes.stencil;
+
+  for (const extension of ['EXT_color_buffer_float']) {
+    if (!gl.getExtension(extension)) {
+      throw new Error(`${extension} extension not available`);
+    }
+  }
+
+  // Extensions to attempt to add but not fail if they are not available.
+  for (const extension of [
+           // Some versions of Firefox 67.0 seem to require this extension being added in addition
+           // to EXT_color_buffer_float, despite the note here indicating it is unnecessary:
+           // https://developer.mozilla.org/en-US/docs/Web/API/EXT_float_blend
+           //
+           // See https://github.com/google/neuroglancer/issues/140
+           'EXT_float_blend',
+  ]) {
+    gl.getExtension(extension);
+  }
+  return gl;
+}
