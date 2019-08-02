96667a09a2e5be9ffa97b414a6b347de4ccf2979
Manuel Castro <macastro@princeton.edu>
2019-08-02 12:05:10 -0400

    Add viewer undo state change function
    Right now only triggered when user deletes a layer, and chooses
    to undo it.

--- src/neuroglancer/viewer.ts
+++ src/neuroglancer/viewer.ts
@@ -63,6 +63,7 @@ import {TrackableScaleBarOptions} from 'neuroglancer/widget/scale_bar';
 import {makeTextIconButton} from 'neuroglancer/widget/text_icon_button';
 import {RPC} from 'neuroglancer/worker_rpc';
 import {findWhatsNew, WhatsNewDialog} from './whats_new/whats_new';
+import {getCachedJson} from 'neuroglancer/util/trackable';
 
 require('./viewer.css');
 require('neuroglancer/noselect.css');
@@ -200,7 +201,7 @@ export class Viewer extends RefCounted implements ViewerState {
   navigationState = this.registerDisposer(new NavigationState());
   perspectiveNavigationState = new NavigationState(new Pose(this.navigationState.position), 1);
   mouseState = new MouseSelectionState();
-  layerManager = this.registerDisposer(new LayerManager());
+  layerManager = this.registerDisposer(new LayerManager(this.getStateRevertingFunction.bind(this)));
   selectedLayer = this.registerDisposer(new SelectedLayerState(this.layerManager.addRef()));
   showAxisLines = new TrackableBoolean(true, true);
   showScaleBar = new TrackableBoolean(true, true);
@@ -774,6 +775,13 @@ export class Viewer extends RefCounted implements ViewerState {
     if (this.visible) {
       this.dataContext.chunkQueueManager.chunkUpdateDeadline = null;
     }
+  }
+
+  private getStateRevertingFunction() {
+    const currentState = getCachedJson(this.state).value;
+    return () => {
+      this.state.restoreState(currentState);
+    };
   }
 
   private handleNavigationStateChanged() {
