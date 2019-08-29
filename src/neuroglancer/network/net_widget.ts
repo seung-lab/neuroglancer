686f2e331a8db4413ada2fad3d251ad0eea70235
Seun Ogedengbe <seun40@gmail.com>
2019-08-29 16:31:44 -0400

    workaholic time

--- src/neuroglancer/network/net_widget.ts
+++ src/neuroglancer/network/net_widget.ts
@@ -10,94 +10,89 @@ require('./net_widget.css');
 
 interface User {
   id: number;
-  chan: Object;
+  chan: any;
   name?: string;
 }
-/*interface ItemConfig {
+
+interface InputConfig {
+  id: string;
+  classes?: string[];
+  value?: string;
+  disabled?: boolean;
   type?: string;
-  id?: string;
-  className?: string;
-}*/
+  checked?: boolean;
+}
 
 interface NetworkPrefs {
   host: string;
   user: User;
-  syncables: Object;
+  syncables: any;
+  syncChan: string;
 }
 
 const br = () => document.createElement('br');
-class NetworkConfiguration extends Overlay {
+const brp = (e: HTMLElement) => [e, br(), br()];
+abstract class NetworkOverlay extends Overlay {
   constructor(public viewer: Viewer, public net: Network) {
     super();
-    let {content} = this;
+  }
 
-    const labelWrap = (label: string, element?: (HTMLElement|string)[], id?: string, ret?: boolean) => {
-      const labelElement = document.createElement('label');
-      if (id) {
-        labelElement.id = id;
-      }
-
-      labelElement.textContent = label;
-      if (element) {
-        element.map(e => labelElement.append(e));
-      }
-      if (ret) {
+  labelWrap =
+      (label: string, element?: (HTMLElement|string)[], config?: InputConfig) => {
+        const labelElement = document.createElement('label');
+        labelElement.textContent = label;
+        if (config) {
+          const {id, classes} = config;
+          labelElement.id = id;
+          labelElement.classList.add(...(classes || []));
+        }
+        if (element) {
+          labelElement.append(...element);
+        }
         return labelElement;
-      } else {
-        modal.appendChild(labelElement);
-        modal.appendChild(br());
-        modal.appendChild(br());
-        return;
       }
-    };
 
-    const simpleInput = (label: string, id: string, element?: (HTMLElement|string)[]) => {
-      const textbox = document.createElement('input');
+  simpleInput =
+      (config: InputConfig) => {
+        const {id, classes, value, disabled, type} = config;
+        const textbox = document.createElement('input');
 
-      textbox.id = id;
-      textbox.type = 'text';
-      labelWrap(label, [' ', textbox, ...(element ? element : [])]);
-    };
+        textbox.id = id;
+        textbox.type = type || 'text';
+        textbox.value = value || '';
+        textbox.disabled = disabled || false;
+        textbox.classList.add(...(classes || []));
+        return textbox;
+      }
 
-    const simpleInputReal = (label: string, id: string, element?: (HTMLElement|string)[]) => {
-      const textbox = document.createElement('input');
+  simpleItem = (config: InputConfig = {id: '', type: 'checkbox'}) => {
+    const {id, classes, value, disabled, type, checked} = config;
+    const chkbox = document.createElement('input');
 
-      textbox.id = id;
-      textbox.type = 'text';
-      return <HTMLLabelElement> labelWrap(label, [' ', textbox, ...(element ? element : [])], void(0), true);
-    };
-
-    /*const simpleItem =
-        (value: string, config: ItemConfig = {type: 'checkbox'}, checked?: boolean) => {
-          const chkbox = document.createElement('input');
-          const {id, className, type} = config;
-
-          if (id) {
-            chkbox.id = id;
-          }
-          if (className) {
-            chkbox.className += className;
-          }
-          chkbox.value = value;
-          chkbox.type = type || 'checkbox';
-
-          if (type === 'radio') {
-            chkbox.name = className || id || '';
-          }
-
-          if (checked) {
-            chkbox.checked = true;
-          }
-
-          return chkbox;
-        };*/
-
-    let modal = document.createElement('div');
+    chkbox.id = id;
+    chkbox.type = type || 'text';
+    chkbox.value = value || '';
+    chkbox.disabled = disabled || false;
+    chkbox.classList.add(...(classes || []));
+    chkbox.checked = checked || false;
+    if (type === 'radio') {
+      chkbox.name = (classes || [])[0] || id || '';
+    }
+    return chkbox;
+  }
+}
+class NetworkConfiguration extends NetworkOverlay {
+  constructor(public viewer: Viewer, public net: Network) {
+    super(viewer, net);
+    const {labelWrap, simpleInput, /*simpleItem,*/ content} = this;
+    const modal = document.createElement('div');
     content.appendChild(modal);
+
     let header = document.createElement('h3');
     header.textContent = 'Network Configuration';
-    modal.appendChild(header);
-
+    const uidField = labelWrap('ID', [
+      ' ', simpleInput({id: 'net-uid', value: net.settings.user.id.toString(), disabled: true})
+    ]);
     const connectBtn = document.createElement('button');
     connectBtn.id = 'net-connect-button';
     connectBtn.innerHTML = 'Connect';
@@ -106,64 +101,100 @@ class NetworkConfiguration extends Overlay {
       net.settings.host = host.value;
       net.connect();
     });
-    simpleInput('ID', 'net-uid');
-    simpleInput('Host', 'net-hostaddr', [' ', connectBtn]);
+    const hostField = labelWrap(
+        'Host',
+        [' ', simpleInput({id: 'net-hostaddr', value: net.settings.host}), ' ', connectBtn]);
     const applyBtn = document.createElement('button');
     applyBtn.innerHTML = 'Apply Changes';
     applyBtn.addEventListener('click', () => this.apply());
-    modal.appendChild(applyBtn);
-    modal.appendChild(br());
-    modal.appendChild(br());
-    simpleInput('Nickname', 'net-nick');
-    const addChan = () => {
-      const textbox = document.createElement('input');
-      textbox.classList.add('net-cid');
-      textbox.type = 'text';
-      return textbox;
-    };
+    const nickField = labelWrap(
+        'Nickname', [' ', simpleInput({id: 'net-nick', value: net.settings.user.name || ''})]);
+
+    let chanArr = <(string | HTMLElement)[]>[br()];
+    let iter = 0;
+    for (let cid in net.settings.user.chan) {
+      let chanFields = this.addChan(iter);
+      if (!iter) {
+        chanFields = [chanFields[0]];
+        (<HTMLInputElement>chanFields[0]).disabled = true;
+      }
+      (<HTMLInputElement>chanFields[0]).value = cid;
+      (<HTMLInputElement>chanFields[0]).dataset['prev'] = cid;
+      chanArr = [...chanArr, ...chanFields, br()];
+    }
+    const chanGroup = labelWrap('Channels', chanArr, {id: 'net-channels-group'});
     const newChannel = document.createElement('button');
-    //Function to create new row of channel with - button except for the first one
     newChannel.id = 'net-addchannel-button';
     newChannel.innerHTML = '+';
     newChannel.addEventListener('click', () => {
-      const host = <HTMLInputElement>document.getElementById('net-hostaddr');
-      net.settings.host = host.value;
-      net.connect();
+      const channels = <HTMLInputElement[]>Array.from(document.querySelectorAll('.net-cid'));
+      const it = channels.length;
+      const chanFields = this.addChan(it);
+      chanGroup.append(...chanFields, br());
     });
-    const channelArr = [];
-    //loop channel add
-    labelWrap('Channels', [br(), simpleInputReal('', 'net-cid-0')]);
+    const syncField = labelWrap('Sync Settings', []);
 
-    // let issueTypeConfig = {type: 'checkbox', className: 'form_type'};
-    labelWrap('Sync Settings', [
-      /*br(), simpleItem('1', issueTypeConfig), ' Bug ', simpleItem('2', issueTypeConfig),
-      ' Suggestion'*/
-    ]);
-
-    const uid = (<HTMLInputElement>document.getElementById('net-uid')!);
-    uid.disabled = true;
-    uid.value = net.settings.user.id.toString();
-    (<HTMLInputElement>document.getElementById('net-hostaddr')!).value = net.settings.host;
-    (<HTMLInputElement>document.getElementById('net-cid')!).value =
-        net.settings.user.chan.toString();
-    (<HTMLInputElement>document.getElementById('net-nick')!).value = net.settings.user.name || '';
+    const buildArr = [
+      header, ...brp(uidField), ...brp(hostField), ...brp(applyBtn), ...brp(nickField), chanGroup,
+      ...brp(newChannel), ...brp(syncField)
+    ];
+    modal.append(...buildArr);
   }
 
   apply() {
-    const opts = {
-      cid: (<HTMLInputElement>document.getElementById('net-cid')!).value,
-      nick: (<HTMLInputElement>document.getElementById('net-nick')!).value
-    };
-
-    if (opts.cid !== '' && parseInt(opts.cid, 10) !== this.net.settings.user.chan) {
-      this.net.settings.user.chan = parseInt(opts.cid, 10);
-      this.net.channel();
-    }
-
-    if (opts.nick !== '' && opts.nick !== this.net.settings.user.name) {
-      this.net.settings.user.name = opts.nick;
+    const nick = (<HTMLInputElement>document.getElementById('net-nick')!).value;
+    if (nick !== '' && nick !== this.net.settings.user.name) {
+      this.net.settings.user.name = nick;
       this.net.nickname();
     }
+
+    const channels = <HTMLInputElement[]>Array.from(document.querySelectorAll('.net-cid'));
+    for (let field of channels) {
+      const val = field.value;
+      if (val !== '' && !this.net.settings.user.chan[val]) {
+        field.dataset['prev'] = field.value;
+        this.net.settings.user.chan[val] = 1;
+        this.net.channel(val, true);
+      }
+    }
+  }
+
+  remChannelBtnBuilder =
+      (id: string) => {
+        const remove = document.createElement('button');
+        remove.innerHTML = '-';
+        remove.classList.add('net-remchannel-button');
+        remove.addEventListener('click', () => {
+          const target = <HTMLInputElement>document.getElementById(id);
+          const key = target.dataset['prev']!;
+          if (key) {
+            this.net.channel(key, false);
+            delete this.net.settings.user.chan[key];
+          }
+        });
+        return remove;
+      }
+
+  addChan = (index: number) => {
+    const textbox = document.createElement('input');
+    textbox.classList.add('net-cid');
+    textbox.id = `cid-idx-${index.toString()}`;
+    textbox.type = 'text';
+    return [textbox, ' ', this.remChannelBtnBuilder(textbox.id)];
+  }
+}
+
+class NetworkHelp extends NetworkOverlay {
+  constructor(public viewer: Viewer, public net: Network) {
+    super(viewer, net);
+    // let {content} = this;
+  }
+}
+
+class NetworkSearch extends NetworkOverlay {
+  constructor(public viewer: Viewer, public net: Network) {
+    super(viewer, net);
+    // let {content} = this;
   }
 }
 
@@ -172,11 +203,12 @@ class NetworkChatWidget extends RefCounted {
   header = document.createElement('div');
   input = <HTMLInputElement>document.createElement('input');
   viewport = <HTMLTextAreaElement>document.createElement('textarea');
+  activeCh = <HTMLSelectElement>document.createElement('select');
 
   constructor(net: Network) {
     super();
 
-    const {element, header, input, viewport} = this;
+    const {element, header, input, viewport, activeCh} = this;
     element.className = 'neuroglancer-net-chatWindow';
     header.className = 'neuroglancer-net-chatHeader';
     viewport.className = 'neuroglancer-net-display';
@@ -185,15 +217,39 @@ class NetworkChatWidget extends RefCounted {
     viewport.cols = 40;
     input.className = 'neuroglancer-net-input';
     input.type = 'text';
+    const defopt = document.createElement('option');
+    defopt.value = '0';
+    defopt.innerHTML = '0(All)';
+    activeCh.appendChild(defopt);
+    activeCh.className = '.neuroglancer-net-channel';
 
     const config = makeTextIconButton('⚙', 'Net Config');
+    const help = makeTextIconButton('?', 'Net Help');
+    const search = makeTextIconButton('🔍', 'Net Query');
+    const notify = makeTextIconButton('🔔', 'Notifications');
+    const empty = makeTextIconButton('');
+    empty.classList.add('empty-button');
     this.registerEventListener(config, 'click', () => {
-      net.open();
+      net.config();
+    });
+    this.registerEventListener(help, 'click', () => {
+      net.nethelp();
+    });
+    this.registerEventListener(search, 'click', () => {
+      net.searchDialog();
+    });
+    this.registerEventListener(notify, 'click', () => {
+      net.notifyToggle();
     });
     header.appendChild(config);
+    header.appendChild(search);
+    header.appendChild(help);
+    header.appendChild(notify);
+    header.appendChild(empty);
     element.appendChild(header);
     element.appendChild(viewport);
     element.appendChild(document.createElement('br'));
+    element.appendChild(activeCh);
     element.appendChild(input);
 
     this.dragElement();
@@ -244,8 +300,9 @@ class NetworkChatWidget extends RefCounted {
 export class Network {
   settings: NetworkPrefs = {
     host: 'wss://seungissues-wbsimp-test3.herokuapp.com/',
-    user: JSON.parse(localStorage.getItem('wbsUser') || '{"id": -1, "chan": 0}'),
-    syncables: {}
+    user: JSON.parse(localStorage.getItem('wbsUser') || '{"id": -1, "chan": {0: true}}'),
+    syncables: {},
+    syncChan: '0'
   };
   chatWindow: NetworkChatWidget;
   ws: WebSocket;
@@ -268,14 +325,11 @@ export class Network {
     const net = this;
     const repState = window.history.replaceState.bind(window.history);
     const stateChangeHandler = () => {
-      if (net.ws && net.ws.readyState === net.ws.OPEN) {
+      if (net.ws && net.ws.readyState === net.ws.OPEN && net.settings.syncChan !== '0') {
         // Do not send state if it was from remote
-        const state = this.viewer.state.toJSON();  // this.stateFromUrl();
+        const state = this.viewer.state.toJSON();
         const stateString = JSON.stringify(state);
-        if (stateString !==
-                net.lastRemoteStateString /*&& stateString !== net.lastLocalStateString*/
-            && stateString !== net.lastSentStateString) {
-          // console.log(this.diff(stateString, net.lastRemoteStateString || ''));
+        if (stateString !== net.lastRemoteStateString && stateString !== net.lastSentStateString) {
           net.lastSentStateString = stateString;
           net.ws.send(JSON.stringify({method: 'state', state}));
         }
@@ -285,13 +339,13 @@ export class Network {
       repState.apply(window.history, args);
       stateChangeHandler();
     };
-    // this.viewer.state.changed.add(stateChangeHandler);
 
     this.chatWindow.input.addEventListener('keyup', (e) => {
       if (e.code === 'Enter' && this.ws) {
         const {input} = this.chatWindow;
         let message: string|string[] = input.value;
         let method;
+        let chan = this.chatWindow.activeCh.value;
         if (message.length && message[0] === '\\') {
           message = message.split('');
           const cmdend = message.indexOf(' ');
@@ -306,21 +360,39 @@ export class Network {
           method.shift();
           method = method.join('');
         }
-        this.ws.send(JSON.stringify({message, method}));
+        this.ws.send(JSON.stringify({message, method, chan}));
         input.value = '';
       }
     });
   }
 
-  open() {
+  config() {
     new NetworkConfiguration(this.viewer, this);
   }
 
-  channel() {
+  nethelp() {
+    new NetworkHelp(this.viewer, this);
+  }
+
+  notifyToggle() {}
+
+  searchDialog() {
+    new NetworkSearch(this.viewer, this);
+  }
+
+  query() {
     if (this.ws) {
-      this.ws.send(JSON.stringify({message: this.settings.user.chan, method: 'channel'}));
+      this.ws.send(
+          JSON.stringify({message: this.settings.user.chan, method: 'query', divert: true}));
     }
   }
+
+  channel(message: string, join: boolean) {
+    if (this.ws) {
+      this.ws.send(JSON.stringify({message, method: (join) ? 'channel' : 'leave'}));
+    }
+  }
+
 
   nickname() {
     if (this.ws) {
