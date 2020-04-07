4832de98dcf82bdb66fa26c58c3615b1fa8e4117
MORGANNA\Seun <seun40@gmail.com>
2020-04-07 14:17:46 -0400

    change to 3 dots

--- src/neuroglancer/viewer.ts
+++ src/neuroglancer/viewer.ts
@@ -523,7 +523,7 @@ export class Viewer extends RefCounted implements ViewerState {
     });
     */
     hbDropdown.classList.add('ng-hb-dropdown');
-    const createMenuItem = (text: string, title?: string) => {
+    const createMenuItem = (text: string,\ title?: string) => {
       const element = document.createElement('li');
       const button = makeTextIconButton(text, title);
       element.append(button);
@@ -532,7 +532,7 @@ export class Viewer extends RefCounted implements ViewerState {
 
     {
       // Hamburger
-      const button = makeTextIconButton('☰', 'Customize and Control Neuroglancer');
+      const button = makeTextIconButton('⋮', 'Customize and Control Neuroglancer');
       button.append(hbDropdown);
       button.classList.add('ng-hamburger');
       /*
