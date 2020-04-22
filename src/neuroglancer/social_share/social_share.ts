cc62018c241f0939ad5904fece9c9770ed5bea8e
MORGANNA\Seun <seun40@gmail.com>
2020-04-21 23:17:18 -0400

    jssocials + zepto

---
+++ src/neuroglancer/social_share/social_share.ts
@@ -0,0 +1,11 @@
+import 'zepto';
+import 'neuroglancer/social_share/jssocials';
+// import 'neuroglancer/save_state/jssocials-theme-classic.css';
+import 'neuroglancer/save_state/jssocials-theme-flat.css';
+// import 'neuroglancer/save_state/jssocials-theme-minimal.css';
+// import 'neuroglancer/save_state/jssocials-theme-plain.css';
+
+export function createSocialBar() {
+  const socials = document.createElement('div');
+  return socials;
+}
