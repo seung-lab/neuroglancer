570afca3a1bf93254fca1f7601ef2ca05a78964c
MORGANNA\Seun <seun40@gmail.com>
2020-12-05 13:37:50 -0500

    rollback works, working on rollfoward

--- src/neuroglancer/save_state/save_diff.ts
+++ src/neuroglancer/save_state/save_diff.ts
@@ -4,7 +4,6 @@ import {Trackable} from 'neuroglancer/util/trackable';
 const diff = new diff_match_patch();
 
 export class SaveDiff {
-  active = true;
   diffsInstances = 0;
   cursor = 0;
   max = 100;
@@ -18,17 +17,15 @@ export class SaveDiff {
     const oldSerial = JSON.stringify(oldState);
     const newSerial = JSON.stringify(newState);
     const stateChange = oldSerial !== newSerial;
-    if (!this.active) {
-      this.active = true;
-      return stateChange;
-    }
+
     if (stateChange) {
-      if (this.cursor < this.stack.length) {
-        this.stack.splice(this.cursor);
-      }
       const patch = diff.patch_toText(diff.patch_make(oldSerial, newSerial));
-      this.stack.push(patch);
-      this.cursor = this.stack.length;
+      if (this.cursor < this.stack.length - 1) {
+        this.stack[this.cursor] = patch;
+      } else {
+        this.cursor = this.stack.push(patch);
+      }
+      // this.cursor++;
     }
     return stateChange;
   }
@@ -50,7 +47,6 @@ export class SaveDiff {
     const restoreFromPatch = diff.patch_apply(patchfromText, currentState);
     /* deactivate so that state change triggered by updating
     the state w/ a rollback doesn't affect state history*/
-    this.active = false;
     this.root.restoreState(JSON.parse(restoreFromPatch[0]));
   }
 }
