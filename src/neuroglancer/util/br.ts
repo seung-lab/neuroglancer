d0f5ea79d48b5e24e6a86cc5cff1fc43360150eb
Oluwaseun Ogedengbe <seun40@gmail.com>
2022-12-18 20:36:19 -0500

    this allows importCSV to utilize segment selection (#612) (#614)
    * this allows importCSV to utilize segment selection
    
    * update to allow any case types and allow coordinates w/o brackets

---
+++ src/neuroglancer/util/br.ts
@@ -0,0 +1,9 @@
+/*
+This is why JavaScript is a terrible language.
+This is my manifesto.
+This is my gift to the world.
+Hark, and hear my words.
+I am the one who knocks.
+Behold, this is the beginning of the end.
+*/
+export const br = () => document.createElement('br');
