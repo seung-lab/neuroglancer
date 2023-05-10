296aa0bf1eb984c585b522708466722755c2b847
Chris Jordan <jordanchriss@gmail.com>
2023-05-10 16:58:46 -0400

    custom keybinds and layer independent tool keybinds

--- src/ui/default_viewer_setup.ts
+++ src/ui/default_viewer_setup.ts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+import type { UserLayer, UserLayerConstructor } from "#src/layer/index.js";
+import { SegmentationUserLayer } from "#src/layer/segmentation/index.js";
 import { StatusMessage } from "#src/status.js";
 import {
   bindDefaultCopyHandler,
@@ -23,9 +25,26 @@ import { setDefaultInputEventBindings } from "#src/ui/default_input_event_bindin
 import { makeDefaultViewer } from "#src/ui/default_viewer.js";
 import type { MinimalViewerOptions } from "#src/ui/minimal_viewer.js";
 import { bindTitle } from "#src/ui/title.js";
+import type { Tool } from "#src/ui/tool.js";
+import { restoreTool } from "#src/ui/tool.js";
 import { UrlHashBinding } from "#src/ui/url_hash_binding.js";
 
 declare let NEUROGLANCER_DEFAULT_STATE_FRAGMENT: string | undefined;
+
+type CustomBinding = {
+  layer: string;
+  tool: string;
+  protocol?: string;
+};
+
+type CustomBindings = {
+  [key: string]: CustomBinding | string;
+};
+
+declare const CUSTOM_BINDINGS: CustomBindings | undefined;
+export const hasCustomBindings =
+  typeof CUSTOM_BINDINGS !== "undefined" &&
+  Object.keys(CUSTOM_BINDINGS).length > 0;
 
 /**
  * Sets up the default neuroglancer viewer.
@@ -33,6 +52,77 @@ declare let NEUROGLANCER_DEFAULT_STATE_FRAGMENT: string | undefined;
 export function setupDefaultViewer(options?: Partial<MinimalViewerOptions>) {
   const viewer = ((<any>window).viewer = makeDefaultViewer(options));
   setDefaultInputEventBindings(viewer.inputEventBindings);
+
+  const bindActionToTool = (
+    action: string,
+    toolType: string,
+    toolKey: string,
+    desiredLayerType: UserLayerConstructor,
+    desiredProtocol?: string,
+  ) => {
+    let previousTool: Tool<object> | undefined;
+    let previousLayer: UserLayer | undefined;
+    viewer.bindAction(action, () => {
+      const acceptableLayers = viewer.layerManager.managedLayers.filter(
+        (managedLayer) => {
+          const correctLayerType =
+            managedLayer.layer instanceof desiredLayerType;
+          if (desiredProtocol && correctLayerType) {
+            for (const dataSource of managedLayer.layer?.dataSources || []) {
+              const protocol = viewer.dataSourceProvider.getProvider(
+                dataSource.spec.url,
+              )[2];
+              if (protocol === desiredProtocol) {
+                return true;
+              }
+            }
+            return false;
+          } else {
+            return correctLayerType;
+          }
+        },
+      );
+      if (acceptableLayers.length > 0) {
+        const firstLayer = acceptableLayers[0].layer;
+        if (firstLayer) {
+          if (firstLayer !== previousLayer) {
+            previousTool = restoreTool(firstLayer, toolType);
+            previousLayer = firstLayer;
+          }
+          if (previousTool) {
+            viewer.activateTool(toolKey, previousTool);
+          }
+        }
+      }
+    });
+  };
+
+  const nameToLayer: { [key: string]: UserLayerConstructor | undefined } = {};
+
+  for (const x of [SegmentationUserLayer]) {
+    nameToLayer[x.type] = x;
+  }
+
+  if (hasCustomBindings) {
+    for (const [key, val] of Object.entries(CUSTOM_BINDINGS!)) {
+      if (typeof val === "string") {
+        viewer.inputEventBindings.global.set(key, val);
+      } else {
+        viewer.inputEventBindings.global.set(key, `tool-${val.tool}`);
+        const layerConstructor = nameToLayer[val.layer];
+        if (layerConstructor) {
+          const toolKey = key.charAt(key.length - 1).toUpperCase();
+          bindActionToTool(
+            `tool-${val.tool}`,
+            val.tool,
+            toolKey,
+            layerConstructor,
+            val.protocol,
+          );
+        }
+      }
+    }
+  }
 
   const hashBinding = viewer.registerDisposer(
     new UrlHashBinding(
