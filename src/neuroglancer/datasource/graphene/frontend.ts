7a870bacf2d41c555c385f8d66e2dab8e9dbd809
Chris Jordan <jordanchriss@gmail.com>
2023-06-14 12:18:42 -0400

    removed delay between removing old root ids and adding new root ids for a merge

--- src/neuroglancer/datasource/graphene/frontend.ts
+++ src/neuroglancer/datasource/graphene/frontend.ts
@@ -1105,10 +1105,10 @@ class GraphConnection extends SegmentationGraphSourceConnection {
           segmentsToAdd.push(submission.mergedRoot);
         }
       }
+      const latestRoots = await this.graph.graphServer.filterLatestRoots(segmentsToAdd);
       const segmentsState = this.layer.displayState.segmentationGroupState.value;
       const {visibleSegments, selectedSegments} = segmentsState;
       selectedSegments.delete(segmentsToRemove);
-      const latestRoots = await this.graph.graphServer.filterLatestRoots(segmentsToAdd);
       this.meshAddNewSegments(latestRoots);
       selectedSegments.add(latestRoots);
       visibleSegments.add(latestRoots);
