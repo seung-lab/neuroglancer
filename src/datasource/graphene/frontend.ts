a04270b6f3078dc8742ad88111ad1720d230e0a9
Chris Jordan <jordanchriss@gmail.com>
2024-05-21 12:31:17 -0400

    feat(datasource/graphene) bumped bulk merge limit to 20

--- src/datasource/graphene/frontend.ts
+++ src/datasource/graphene/frontend.ts
@@ -3025,7 +3025,7 @@ function mergeToLine(submission: MergeSubmission): Line {
   return res;
 }
 
-const MAX_MERGE_COUNT = 10;
+const MAX_MERGE_COUNT = 20;
 
 const MERGE_SEGMENTS_INPUT_EVENT_MAP = EventActionMap.fromObject({
   "at:shift?+enter": { action: "submit" },
