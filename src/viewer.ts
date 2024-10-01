5398081772dc769a0a8e65170e4b0e75e1e63e00
Chris Jordan <jordanchriss@gmail.com>
2024-10-01 11:38:19 -0400

    add keybinds (alt+up arrow, alt+down arrow) to iterate through the current active layer's annotation list

--- src/viewer.ts
+++ src/viewer.ts
@@ -1053,6 +1053,21 @@ export class Viewer extends RefCounted implements ViewerState {
       });
     }
 
+    const sendEventToSelectedLayer = (type: string) => {
+      const selectedLayer = this.selectedLayer.layer?.layer;
+      if (selectedLayer) {
+        selectedLayer.dispatchLayerEvent(type);
+      }
+    };
+
+    this.bindAction("select-previous", () => {
+      sendEventToSelectedLayer("select-previous");
+    });
+
+    this.bindAction("select-next", () => {
+      sendEventToSelectedLayer("select-next");
+    });
+
     for (const action of ["select", "star"]) {
       this.bindAction(action, () => {
         this.mouseState.updateUnconditionally();
