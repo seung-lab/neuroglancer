2556d44570921c225c1869f96a32ac48bb2454b8
raphaellevisse <raphael_levisse@berkeley.edu>
2024-12-20 11:08:26 -0500

    Modifications to recording, rl-recording branch

--- src/ui/default_viewer_setup.ts
+++ src/ui/default_viewer_setup.ts
@@ -193,18 +193,23 @@ export function setupDefaultViewer() {
   };
 
   (window as any).saveHistory = (sessionId: string) => {
-    const res: any[] = [];
+    const historyData: any[] = [];
     let historyIndex = 0;
     let state: string | null = null;
     while (
-      (state = localStorage.getItem(`${sessionId}_${historyIndex}`)) !== null
+      (state = localStorage.getItem(`history_state_${sessionId}_${historyIndex}`)) !== null
     ) {
-      res.push(JSON.parse(state));
+      const action = localStorage.getItem(`history_action_${sessionId}_${historyIndex}`);
+      const timestamp = localStorage.getItem(`history_time_${sessionId}_${historyIndex}`);
+      historyData.push({
+        state: JSON.parse(state),
+        action: action,
+        time: timestamp
+      });
       historyIndex++;
     }
-    downloadObject(res, "history.json");
+    downloadObject(historyData, `episode_${sessionId}.json`);
   };
-
   let inReplay = false;
 
   const ngReplay = (sessionId: string, skipForward = true) => {
@@ -212,6 +217,8 @@ export function setupDefaultViewer() {
     hashBinding.recording = false;
     inReplay = true;
     let historyIndex = 0;
+    const preReplayState = viewer.state.toJSON();
+    console.log("pre replay state", preReplayState);
     let nextState = localStorage.getItem(
       `history_state_${sessionId}_${historyIndex}`,
     );
@@ -226,20 +233,50 @@ export function setupDefaultViewer() {
     let startTime = Date.now() - nextTime;
 
     console.log("starting replay");
-    StatusMessage.showTemporaryMessage("starting replay");
+
+    let mouseMoved = false;
+
+    const handleMouseMove = () => {
+      mouseMoved = true;
+    };
+    // Mouse movement changes the internal state (cf index.ts) and will cause the replay state to fail because it does consistency of the browser is lost
+    // Thus why mouse movement instantly stops the replay
+    window.addEventListener("mousemove", handleMouseMove);
 
     const loop = () => {
+      if (mouseMoved) {
+        console.log("Mouse movement detected. Stopping replay.");
+        window.removeEventListener("mousemove", handleMouseMove);
+        inReplay = false;
+        StatusMessage.showTemporaryMessage("Replay stopped due to mouse movement.");
+        viewer.state.restoreState(preReplayState);
+        return;
+      }
+
       let elapsedTime = Date.now() - startTime;
-      if (skipForward && nextTime - elapsedTime > 5000) {
-        startTime -= nextTime - elapsedTime - 5000;
+      if (skipForward && nextTime - elapsedTime > 1000) {
+        startTime -= nextTime - elapsedTime - 1000;
         elapsedTime = Date.now() - startTime;
         // console.log("jumping forward");
       }
+      //console.log("elapsed time", elapsedTime);
+      //console.log(mouseData.length, mouseIndex);
+      
       // console.log("replay", elapsedTime, historyIndex, nextTime - elapsedTime);
       if (nextTime > 0 && elapsedTime >= nextTime) {
-        // set new state
-        viewer.state.restoreState(JSON.parse(nextState!));
-        // get next states
+        console.log("New frame", historyIndex);
+        const actionKey = `history_action_${sessionId}_${historyIndex}`;
+        const actionEvent = localStorage.getItem(actionKey);
+        console.log(`${actionEvent}`);
+        const parsedState = JSON.parse(nextState!);
+        try {
+          viewer.state.restoreState(parsedState);
+        }
+        catch (e) {
+          inReplay = false;
+          console.error(e);
+          return;
+        }
         historyIndex++;
         nextState = localStorage.getItem(
           `history_state_${sessionId}_${historyIndex}`,
@@ -247,13 +284,15 @@ export function setupDefaultViewer() {
         if (!nextState) {
           inReplay = false;
           console.log("done with replay");
-          StatusMessage.showTemporaryMessage("replay  complete!");
+          StatusMessage.showTemporaryMessage("replay complete!");
+          window.removeEventListener("mousemove", handleMouseMove);
           return;
         }
         nextTime = parseInt(
           localStorage.getItem(`history_time_${sessionId}_${historyIndex}`)!,
         );
       }
+      //await new Promise(resolve => setTimeout(resolve, 1000));
       requestAnimationFrame(loop);
     };
     requestAnimationFrame(loop);
@@ -263,9 +302,8 @@ export function setupDefaultViewer() {
 
   let currentOverlay: Overlay | undefined = undefined;
 
-  document.addEventListener("keypress", (evt) => {
-    console.log(evt.key, evt.ctrlKey);
-    if (evt.key === "i") {
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "r" && evt.ctrlKey) {
       if (!currentOverlay || currentOverlay.wasDisposed) {
         currentOverlay = showHistoryViewer();
       } else {
@@ -291,6 +329,30 @@ export function setupDefaultViewer() {
     title.classList.add("title");
     title.textContent = "History";
     historyViewer.append(title);
+
+    const buttonRecord = document.createElement("button");
+    buttonRecord.classList.add("record-button");
+    buttonRecord.textContent = "Record";
+    historyViewer.appendChild(buttonRecord);
+
+    const updateRecordButtonState = () => {
+      if (hashBinding.recording) {
+        buttonRecord.classList.add("active");
+      } else {
+        buttonRecord.classList.remove("active");
+      }
+    };
+    buttonRecord.addEventListener("click", () => {
+      hashBinding.recording = !hashBinding.recording;
+      if (!hashBinding.recording) {
+        console.log("Resetting session ID and history index to prepare for new recording");
+        hashBinding.resetSessionId();
+        hashBinding.resetHistoryIndex();
+      }
+      console.log(hashBinding.recording ? "Recording started" : "Recording stopped");
+      updateRecordButtonState();
+    });
+    updateRecordButtonState(); // sets the initial state
 
     const buttonClose = document.createElement("button");
     buttonClose.classList.add("close-button");
@@ -331,8 +393,10 @@ export function setupDefaultViewer() {
       const count = historyCounts[key];
       const lastUpdateTime = historyTimes[key];
 
-      if (key === hashBinding.sessionId) continue;
-
+      if (key === hashBinding.sessionId) {
+        console.log("Skipping current session", key);
+        continue;
+      }
       if (count < 10) {
         deleteHistory(key);
         continue;
@@ -368,7 +432,17 @@ export function setupDefaultViewer() {
         showHistoryViewer();
       });
       listEl.appendChild(deleteHistoryBtn);
-
+      
+      const saveButton = document.createElement("button");
+      saveButton.textContent = "Save";
+      saveButton.classList.add("save-button");
+      saveButton.addEventListener("click", () => {
+        //const sessionId = "mySession"; // Replace this with the dynamic session ID, if needed
+        console.log('Saving history for session:', key);
+        //saveHistory(sessionId);
+        (window as any).saveHistory(key);
+      });
+      listEl.appendChild(saveButton);
       idx++;
     }
 
--- src/ui/url_hash_binding.ts
+++ src/ui/url_hash_binding.ts
@@ -69,12 +69,28 @@ export class UrlHashBinding extends RefCounted {
 
   private defaultFragment: string;
 
-  readonly sessionId = self.crypto.randomUUID();
+  sessionId = self.crypto.randomUUID();
 
   historyIndex = 0;
+  
   startTime = Date.now();
+  resetSessionId() {
+    this.sessionId = self.crypto.randomUUID();
+  }
+  resetHistoryIndex() {
+    this.historyIndex = 0;
+  }
+  
+  private _recording: boolean;
 
-  recording = true;
+  get recording(): boolean {
+    return this._recording;
+  }
+
+  set recording(isRecording: boolean) {
+    this._recording = isRecording;
+    localStorage.setItem("recording", isRecording.toString());
+  }
 
   constructor(
     public root: Trackable,
@@ -83,16 +99,203 @@ export class UrlHashBinding extends RefCounted {
   ) {
     super();
 
-    const { updateDelayMilliseconds = 200, defaultFragment = "{}" } = options;
-    this.registerEventListener(window, "hashchange", () =>
-      this.updateFromUrlHash(),
-    );
+    /**
+      * In the constructor (called upon refresh), we verify existence of recording state and get it back if possible
+      */
+    const storedRecording = localStorage.getItem("recording");
+    this._recording = storedRecording !== null ? storedRecording === "true" : false;
+    let isDragging = false; 
+    let clickTimeout: number | null = null; 
+    let isSingleClick = true;
+
+
+    const activeKeys = new Set<string>();
+
+    const trackKeyDown = (event: KeyboardEvent) => {
+      activeKeys.add(event.key);
+    };
+
+    const trackKeyUp = (event: KeyboardEvent) => {
+      activeKeys.delete(event.key);
+    };
+
+    const mouseEventListener = (event: MouseEvent, clickTarget: string = '') => {
+
+      const handleClick = (eventType: string) => {
+        let buttonType: string;
+        const keysPressed = Array.from(activeKeys).join(", ") || "None";
+
+        if (event.button === 0) {
+          buttonType = "Left Click";
+        } else if (event.button === 1) {
+          buttonType = "Middle Click";
+        } else if (event.button === 2) {
+          buttonType = "Right Click";
+        } else {
+          buttonType = "Unknown Button";
+        }
+    
+        if (clickTarget !== '') {
+          // means we clicked on important parts
+          //console.log(`Inside render: ${eventType}: ${buttonType} | ${clickTarget} with keys: ${keysPressed} `);
+          throttledSetUrlHash(`Inside render: ${eventType}: ${buttonType} | ${clickTarget} with keys: ${keysPressed} `);
+        }
+        else {
+          //console.log('Don\'t care where you clicked, JSON will catch it if it was important');
+          throttledSetUrlHash(
+            `Outside render: ${eventType}: ${buttonType} with keys: ${keysPressed}`
+        );
+      }
+      };
+      if (event.button === 1) {
+        handleClick("Single Click");
+      } else if (event.button === 2) {
+        handleClick("Single Click");
+      } 
+      else if (event.button === 0) {
+        // logic for double click detection
+        if (clickTimeout) {
+          //console.log("Received second click in window of time");
+          clearTimeout(clickTimeout);
+          clickTimeout = null;
+          isSingleClick = false; 
+          handleClick("Double Click");
+        } else {
+          isSingleClick = true;
+          clickTimeout = window.setTimeout(() => {
+            clickTimeout = null;
+            if (isSingleClick) {
+              handleClick("Single Click");
+            }
+          }, 200); // in ms, time to detect double click
+          }
+        }
+      else {
+        console.log("Unknown mouse button clicked");
+      }
+      };
+    
+    
+    const mouseDownHandler = (event: MouseEvent) => {
+
+      if (event.button === 0) {  // Only track left mouse button
+        isDragging = true;
+        // prevClientX = event.clientX;
+        // prevClientY = event.clientY;
+        const mouseMoveHandler = () => {
+          if (!isDragging) {
+            return;
+          }
+          //console.log("Drag event triggered");
+          // const deltaX = e.clientX - prevClientX;
+          // const deltaY = e.clientY - prevClientY;
+          // prevClientX = e.clientX;
+          // prevClientY = e.clientY;
+          // Triggers the throttled hash update during dragging, 
+          // Careful throttled dragging is too slow to actually be able to rebuild
+          // the state correctly (+ R3 rotations are a non abelian group, meaning we can't just take the cumulated values of dragging)
+          // -> current hack is to record the state through the JSON instead of the deltas X and Y of the mouse
+          throttledSetUrlHash(`Drag`);
+        };
+
+        const mouseUpHandler = (e: PointerEvent) => {
+          if (e.button === event.button) {
+            // End dragging when mouse button is released
+            isDragging = false;
+            //console.log("Drag ended");
+            window.removeEventListener("pointermove", mouseMoveHandler);
+            window.removeEventListener("pointerup", mouseUpHandler);
+          }
+        };
+        // Add event listeners to track mouse movement and stop when mouse button is released
+        window.addEventListener("pointermove", mouseMoveHandler, true);
+        window.addEventListener("pointerup", mouseUpHandler, true);
+      }
+    };
+
+    const wheelHandler = () => {
+      // const deltaX = event.deltaX;
+      // const deltaY = event.deltaY;
+      // const deltaZ = event.deltaZ;
+      throttledSetUrlHash(`Wheel`);
+    };
+
+    this.registerEventListener(window, "wheel", () => {
+      // console.log("Event listener got a wheel event triggered");
+      wheelHandler();
+    }, true);
+
+    const keyboardEventListener = (event: KeyboardEvent) => {
+      //console.log("Key is pressed: ", event.key);
+      throttledSetUrlHash(`Keyboard: ${event.type}`);
+    };
+    
+    this.registerEventListener(window, "dblclick", (event: MouseEvent) => {
+      // this function doesn't work very well as double click is often seen as just two simple clicks
+      // console.log("Double click event triggered");
+      mouseEventListener(event);
+    });
+    
+    this.registerEventListener(window, "mousedown", (event: MouseEvent) => {
+      // this corresponds to dragging events
+      //console.log("Mouse down event triggered");
+      mouseDownHandler(event);
+    }, true);
+    
+    this.registerEventListener(window, "mouseup", (event: MouseEvent) => {
+      const clickedElement =event.target;
+      let clickTarget = "";
+      //console.log("Mouse up event triggered", clickedElement);
+      if (clickedElement instanceof HTMLElement) {
+        //console.log("Exact element clicked:", clickedElement);
+        //console.log("Tag name:", clickedElement.tagName);
+        //console.log("Class list:", clickedElement.className);
+        //.log("ID:", clickedElement.id);
+
+        const parent = clickedElement.closest('.neuroglancer-layer-group-viewer');
+        if (parent) {
+          const rect = parent.getBoundingClientRect();
+          const relativeX = event.clientX - rect.left;
+          const relativeY = event.clientY - rect.top;
+          clickTarget = `Relative position: x=${relativeX}, y=${relativeY}`;
+          //console.log("Clicked inside layer group viewer");
+        }
+        else {
+          //console.log('Clicked on something else');
+          clickTarget = '';
+        }
+      mouseEventListener(event, clickTarget);  
+      }
+    }, true);
+
+
+    this.registerEventListener(window, "keydown", (event: KeyboardEvent) => {
+      trackKeyDown(event);
+      keyboardEventListener(event);
+    });
+
+    this.registerEventListener(window, "keyup", (event: KeyboardEvent) => {
+      trackKeyUp(event);
+      keyboardEventListener(event);
+    });
+    
+    const { updateDelayMilliseconds = 50, defaultFragment = "{}" } = options;
+
+    this.registerEventListener(window, "hashchange", () => {
+      this.updateFromUrlHash();
+    });
+    window.addEventListener("blur", () => {
+      activeKeys.clear(); // Clear active keys when focus is lost, like clicking developer tools f.e.
+    });
+
     const throttledSetUrlHash = debounce(
-      () => this.setUrlHash(),
+      (action_name: string) => {
+        this.setUrlHash(action_name);
+      },
       updateDelayMilliseconds,
-      { maxWait: updateDelayMilliseconds },
+      { maxWait: updateDelayMilliseconds }
     );
-    this.registerDisposer(root.changed.add(throttledSetUrlHash));
+    // this.registerDisposer(root.changed.add(throttledSetUrlHash)); // Original line that triggers state update upon change, however we need associated user actions.
     this.registerDisposer(() => throttledSetUrlHash.cancel());
     this.defaultFragment = defaultFragment;
 
@@ -106,8 +309,9 @@ export class UrlHashBinding extends RefCounted {
   /**
    * Sets the URL hash to match the current state.
    */
-  setUrlHash(saveOnly = false) {
-    const cacheState = getCachedJson(this.root);
+  
+  setUrlHash(action_name= "user_action", saveOnly = false) {
+    const cacheState = getCachedJson(this.root)
     const { generation } = cacheState;
     if (generation !== this.prevStateGeneration) {
       this.prevStateGeneration = cacheState.generation;
@@ -123,11 +327,12 @@ export class UrlHashBinding extends RefCounted {
           }
         }
         if (this.recording) {
-          console.log("recording", this.sessionId, this.historyIndex);
-          localStorage.setItem(
-            `history_elapsed_${this.sessionId}_${this.historyIndex}`,
-            (Date.now() - this.startTime).toString(),
-          );
+          console.log("recording", this.sessionId, this.historyIndex, "Action Event: " + action_name);
+          // not directly useful, technically can be obtained with history_time
+          // localStorage.setItem(
+          //   `history_elapsed_${this.sessionId}_${this.historyIndex}`,
+          //   (Date.now() - this.startTime).toString(),
+          // );
           localStorage.setItem(
             `history_time_${this.sessionId}_${this.historyIndex}`,
             Date.now().toString(),
@@ -135,6 +340,11 @@ export class UrlHashBinding extends RefCounted {
           localStorage.setItem(
             `history_state_${this.sessionId}_${this.historyIndex}`,
             jsonString,
+          );
+          //console.log("history_state", jsonString);
+          localStorage.setItem(
+            `history_action_${this.sessionId}_${this.historyIndex}`,
+            "Action Event: " + action_name,
           );
           this.historyIndex++;
         }
@@ -147,6 +357,7 @@ export class UrlHashBinding extends RefCounted {
    * on the URL hash, then this should be called immediately after construction.
    */
   updateFromUrlHash(upgradeState: (a: any) => any = (x) => x) {
+    console.log("updateFromUrlHash called");
     try {
       let s = location.href.replace(/^[^#]+/, "");
       if (s === "" || s === "#" || s === "#!") {
