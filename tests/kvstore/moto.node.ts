1d574ec6b7219d5dca05b4939d63f9d9bc3c0ebf
Chris Jordan <jordanchriss@gmail.com>
2025-09-22 11:33:33 -0400

    try moto without vitest

---
+++ tests/kvstore/moto.node.ts
@@ -0,0 +1,54 @@
+import { spawn } from "node:child_process";
+// import { describe, test } from "vitest";
+
+export const startMotoServer = async () => {
+    const test1 = spawn(
+      "uv",
+      [
+        "--project",
+        "build_tools/vitest/python_tools",
+        "run",
+        "moto_server",
+        "-p",
+        "0",
+      ],
+      { stdio: ["ignore", "pipe", "pipe"] },
+    );
+
+    test1.stderr.on("data", (data) => {
+      console.log("stderr: ", data.toString());
+    });
+
+    test1.stdout.on("data", (data) => {
+      console.log("stdout: ", data.toString());
+    });
+
+    test1.on("error", (err) => {
+      console.error("test1 process error", err);
+    });
+    test1.on("exit", (code, signal) => {
+      console.log(`test1 process exited with code ${code}, signal ${signal}`);
+    });
+    test1.on("message", (message) => {
+      console.log("test1 process message", message);
+    });
+    test1.on("spawn", () => {
+      console.log("test1 process spawned");
+      setTimeout(() => {
+        console.log("kill test1");
+        test1.kill();
+      }, 10000);
+    });
+    const waitForClose = new Promise<void>((resolve) => {
+      test1.on("close", (code, signal) => {
+        console.log(`test1 process closed with code ${code}, signal ${signal}`);
+        resolve();
+      });
+    });
+    await waitForClose;
+};
+  
+startMotoServer().catch((err) => {
+  console.error("Error starting moto server:", err);
+  process.exit(1);
+});
--- tests/kvstore/moto.spec.ts
+++ tests/kvstore/moto.spec.ts
@@ -1,52 +1,9 @@
-import { spawn } from "node:child_process";
 import { describe, test } from "vitest";
+import { startMotoServer } from "#tests/kvstore/moto.node.js";
 
 describe("describe1", () => {
   test("test1", async () => {
-    const test1 = spawn(
-      "uv",
-      [
-        "--project",
-        "build_tools/vitest/python_tools",
-        "run",
-        "moto_server",
-        "-p",
-        "0",
-      ],
-      { stdio: ["ignore", "pipe", "pipe"] },
-    );
-
-    test1.stderr.on("data", (data) => {
-      console.log("stderr: ", data.toString());
-    });
-
-    test1.stdout.on("data", (data) => {
-      console.log("stdout: ", data.toString());
-    });
-
-    test1.on("error", (err) => {
-      console.error("test1 process error", err);
-    });
-    test1.on("exit", (code, signal) => {
-      console.log(`test1 process exited with code ${code}, signal ${signal}`);
-    });
-    test1.on("message", (message) => {
-      console.log("test1 process message", message);
-    });
-    test1.on("spawn", () => {
-      console.log("test1 process spawned");
-      setTimeout(() => {
-        console.log("kill test1");
-        test1.kill();
-      }, 10000);
-    });
-    const waitForClose = new Promise<void>((resolve) => {
-      test1.on("close", (code, signal) => {
-        console.log(`test1 process closed with code ${code}, signal ${signal}`);
-        resolve();
-      });
-    });
-    await waitForClose;
+    await startMotoServer();
   }, {
     timeout: 100000,
   });
