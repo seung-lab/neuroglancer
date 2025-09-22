a0819a69705e89f328e19e30d5e66e38e8eaf261
Chris Jordan <jordanchriss@gmail.com>
2025-09-22 09:37:31 -0400

    running moto with spawn

---
+++ test_moto_server.ts
@@ -0,0 +1,37 @@
+import { spawn } from "child_process";
+
+const PYTHON_TEST_TOOLS_PATH = "build_tools/vitest/python_tools";
+
+const test1 = spawn(
+    "uv",
+    ["--project", PYTHON_TEST_TOOLS_PATH, "run", "moto_server", "-p", "0"],
+    { stdio: ["ignore", "pipe", "pipe"] },
+);
+
+test1.stderr.on("data", (data) => {
+  console.log("stderr: ", data.toString());
+});
+
+test1.stdout.on("data", (data) => {
+  console.log("stdout: ", data.toString());
+});
+
+test1.on("error", (err) => {
+  console.error("test1 process error", err);
+});
+test1.on("exit", (code, signal) => {
+  console.log(`test1 process exited with code ${code}, signal ${signal}`);
+});
+test1.on("close", (code, signal) => {
+  console.log(`test1 process closed with code ${code}, signal ${signal}`);
+});
+test1.on("message", (message) => {
+  console.log("test1 process message", message);
+});
+test1.on("spawn", () => {
+  console.log("test1 process spawned");
+});
+
+setTimeout(() => {
+  test1.kill();
+}, 100000);
